f84cbbbde2b19a61aae39aec1bcde214f30eaf32
Mikhail Andreev (adw0rd) <x11org@gmail.com>
2013-03-03 01:11:09 +0400

    Remove nose, added Django==1.5 for TravisCI

--- goto_url/tests.py
+++ goto_url/tests.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 import unittest
-from nose.tools import eq_
 
 import utils
 
@@ -8,16 +7,15 @@ import utils
 class TestGotoUrl(unittest.TestCase):
     def test_simple_url(self):
         url = "http://google.com/"
-        eq_(utils.goto_url(url), '/goto/aHR0cDovL2dvb2dsZS5jb20v')
+        self.assertTrue(utils.goto_url(url), '/goto/aHR0cDovL2dvb2dsZS5jb20v')
 
     def test_cyrillic_url(self):
         url = u"http://яндекс.рф/"
-        eq_(utils.goto_url(url), '/goto/aHR0cDovL9GP0L3QtNC10LrRgS7RgNGELw==')
+        self.assertTrue(utils.goto_url(url), '/goto/aHR0cDovL9GP0L3QtNC10LrRgS7RgNGELw==')
 
 
 if __name__ == '__main__':
     unittest.main()
 
 # To run the test suite
-# pip install nose
-# python tests.py -v
+# python tests.py
